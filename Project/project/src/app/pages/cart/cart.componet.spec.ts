b1e01c332baa1131ee1e8a6b9fda866d81dd9e02
Adrián Molina <adrimo100@gmail.com>
2023-03-22 11:19:24 +0100

    inicial

---
+++ Project/project/src/app/pages/cart/cart.componet.spec.ts
@@ -0,0 +1,12 @@
+import { CartComponent } from "./cart.component"
+import { ComponentFixture } from "@angular/core/testing"
+ 
+
+describe("Cart component", () => { //Método donde vamos a crear los test. Primer argumento el nombre del bloque y el ségundo método que contiene los tests
+
+    let component: CartComponent;
+    let fixture: ComponentFixture<CartComponent>
+
+
+
+})
